9f67e800be7b1cbd4b82339e72afd791adb70fb8
Guillermo Becerra <guillermo.ventasclaro@gmail.com>
2023-07-03 08:33:30 -0500

    first commit

---
+++ src/main/java/ec2guillermobecerragroup/ec2guillermobecerra/Ec2guillermobecerraApplication.java
@@ -0,0 +1,13 @@
+package ec2guillermobecerragroup.ec2guillermobecerra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2guillermobecerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2guillermobecerraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec2guillermobecerragroup/ec2guillermobecerra/Ec2guillermobecerraApplicationTests.java
@@ -0,0 +1,13 @@
+package ec2guillermobecerragroup.ec2guillermobecerra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2guillermobecerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
